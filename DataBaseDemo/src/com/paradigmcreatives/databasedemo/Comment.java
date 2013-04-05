00f331ac06b9a1573aad2089a791905ea2186a26
sandeep87 <sandeep.ayipilla@paradigmcreatives.com>
2013-04-05 11:59:59 +0530

    databasedemo

---
+++ DataBaseDemo/src/com/paradigmcreatives/databasedemo/Comment.java
@@ -0,0 +1,31 @@
+package com.paradigmcreatives.databasedemo;
+
+public class Comment {
+	private long id;
+	  private String comment;
+	  public Comment(){		 
+	  }
+
+	  public long getId() {
+	    return id;
+	  }
+
+	  public void setId(long id) {
+	    this.id = id;
+	  }
+
+	  public String getComment() {
+	    return comment;
+	  }
+
+	  public void setComment(String comment) {
+	    this.comment = comment;
+	  }
+
+	  // Will be used by the ArrayAdapter in the ListView
+	  @Override
+	  public String toString() {
+	    return comment;
+	  }
+
+}
