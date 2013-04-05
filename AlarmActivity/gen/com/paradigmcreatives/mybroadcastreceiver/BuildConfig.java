1e241b730f491a0238d8aec9af3f19021ef131a6
sandeep87 <sandeep.ayipilla@paradigmcreatives.com>
2013-04-05 13:07:38 +0530

    alarmactivity

---
+++ AlarmActivity/gen/com/paradigmcreatives/mybroadcastreceiver/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.paradigmcreatives.mybroadcastreceiver;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
